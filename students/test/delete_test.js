f108406cd56b587dbd172369de3cab589b7d66af
dlithepurushottam <dlithepurushottam@gmail.com>
2025-03-17 17:46:42 +0530

    validation and error handling with catch block in backend

---
+++ students/test/delete_test.js
@@ -0,0 +1,36 @@
+const assert=require("assert");
+const Student=require("../src/student");
+
+describe('delete the records',()=>{
+    let  jason;
+
+    beforeEach((done)=>{
+        jason = new Student({name:'Jason'})
+        jason.save()
+        .then(() => done());
+    })
+    it('delete by  id',done =>{
+        Student.findByIdAndDelete(jason._id)
+        .then(()=>Student.findOne({name:'Jason'}))
+        .then((student)=>{
+            assert(student === null)
+            done()
+        })
+    })
+    it('delete by  name',done =>{
+        Student.findOneAndDelete({name:'Jason'})
+        .then(()=>Student.findOne({_id:jason._id}))
+        .then((student)=>{
+            assert(student === null)
+            done()
+        })
+    })
+    it('Delete Jason',done =>{
+        Student.findOne({_id: jason._id})
+        .then(()=>Student.findOne({name:'Rosa'}))
+        .then((student)=>{
+            assert(student === null)
+            done()
+       })
+    })
+})
---
+++ students/test/update_test.js
@@ -0,0 +1,44 @@
+const assert = require("assert");
+const Student = require("../src/student");
+
+describe('Updating records', function () {
+    let jason;
+    let jason2;
+
+
+    beforeEach((done) => {
+        jason = new Student({ name: 'Jason',studentNumber:2500,articleCount:5, grade:10 });
+        jason.save()
+            .then(() => done())
+    });
+
+    // it('set and save', (done) => {
+    //     console.log("Before update:", jason);
+    //     jason.set('name', 'Alex');
+
+    //     jason.save()
+    //         .then(() => Student.find({}))
+    //         .then((students) => {
+    //             console.log("After update:", students);
+    //             assert(students[0].name === "Alex"); 
+    //             done();
+    //         })
+    // });
+
+   
+    // it('update many of the jasons',async()=>{
+    //     const student = await Student.updateMany({name:'Jason'},{studentNumber:3000})
+    //     const res =await Student.find({})
+    //     assert(res[0].studentNumber === 3000 && res[1].studentNumber === 3000)
+    //     console.log(res)
+    // })
+
+    it('Update grades',async()=>{
+        const artCont =await Student.findOne({name:'Jason'})
+        const student = await Student.updateOne({name:'Jason'},{$mul:{grade:artCont.articleCount}})
+        const res= await Student.find({name:'Jason'})
+        assert(res[0].grade === 50)
+        console.log(res)
+    })
+
+});
